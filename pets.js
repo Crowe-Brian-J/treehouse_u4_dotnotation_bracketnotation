680c88794f5c4bf52e9792afa5be0be287b67496
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-01 22:00:27 -0400

    work with syntactic sugar update

--- pets.js
+++ pets.js
@@ -1,3 +1,4 @@
+/* They're changing what I'm doing with the code, so I'm commenting this out
 const ernie = {
   animal: 'dog',
   age: '1',
@@ -17,3 +18,47 @@ const prop = 'breed'
 console.log(ernie[prop])
 
 //when self-referencing inside the object, call *this* instead of object's name like this.animal instead of ernie.animal
+
+ernie.age = 2
+ernie['age'] = 2
+
+ernie.color = 'black'
+
+console.log(ernie)
+*/
+
+const ernie = {
+  animal: 'dog',
+  age: '1',
+  breed: 'pug',
+  bark: function () {
+    console.log('Woof!')
+  }
+}
+
+const vera = {
+  animal: 'dog',
+  age: 8,
+  breed: 'Border Collie',
+  bark: function () {
+    console.log('Woof!')
+  }
+}
+
+const scofield = {
+  animal: 'dog',
+  age: 6,
+  breed: 'Doberman',
+  bark: function () {
+    console.log('Woof!')
+  }
+}
+
+const edel = {
+  animal: 'dog',
+  age: 7,
+  breed: 'German Shorthaired Pointer',
+  bark: function () {
+    console.log('Woof!')
+  }
+}
