12048b66a7f315332b4fa7defbb24a49724d18e2
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-04 19:03:17 -0400

    getter method for Pet activity

--- Pet.js
+++ Pet.js
@@ -10,6 +10,19 @@ class Pet {
     this.sound = sound
   }
 
+  //add activity for Pet - getter method
+  get activity() {
+    const today = new Date()
+    const hour = today.getHours()
+
+    //if to figure out what the pet is doing
+    if (hour > 8 && hour <= 20) {
+      return 'playing'
+    } else {
+      return 'sleeping'
+    }
+  }
+
   //Pet method for speak - outside of constructor
   speak() {
     console.log(this.sound)
@@ -22,3 +35,6 @@ const vera = new Pet('Vera', 'dog', 8, 'Border Collie', 'Woof! Woof!')
 
 ernie.speak()
 vera.speak()
+
+//do not access getter method like speak method - also will not work with console.log(ernie)
+console.log(ernie.activity)
