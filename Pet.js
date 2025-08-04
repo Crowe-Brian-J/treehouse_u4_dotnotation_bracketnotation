9de795af3a3f7c74615c4bac374a4a802b78c7a4
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-04 18:51:23 -0400

    class methods

--- Pet.js
+++ Pet.js
@@ -1,16 +1,24 @@
 //new class - more general than just dogs
 class Pet {
   //constructor method
-  constructor(animal, age, breed) {
+  constructor(name, animal, age, breed, sound) {
     //syntax is this.insert-property-or-method-name-here
+    this.name
     this.animal = animal
     this.age = age
     this.breed = breed
+    this.sound = sound
+  }
+
+  //Pet method for speak - outside of constructor
+  speak() {
+    console.log(this.sound)
   }
 }
 
 //create an instance of a class
-const ernie = new Pet('dog', 1, 'Pug')
-const vera = new Pet('dog', 8, 'Border Collie')
+const ernie = new Pet('Ernie', 'dog', 1, 'Pug', 'Yip! Yip!')
+const vera = new Pet('Vera', 'dog', 8, 'Border Collie', 'Woof! Woof!')
 
-console.log(ernie)
+ernie.speak()
+vera.speak()
